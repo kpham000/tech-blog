2085f45ef66b10260f90d226ea4900ad5ac54492
kpham000 <90490117+kpham000@users.noreply.github.com>
2021-11-01 23:25:29 -0700

    handlebars-models

--- server.js
+++ server.js
@@ -1,30 +1,36 @@
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
+const exphbs = require('express-handlebars');
 
-const sequelize = require('./config/connection???');
+const sequelize = require('./config/connection.js');
 const sequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const hbs = exphbs.create({});
 
-const sess = {
-    secret: 'Super secret secret',
-    cookie: {},
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.static("public"));
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
-    store: new SequelizeStore({
-      db: sequelize
-    })
-  };
-  
-  app.use(session(sess));
-  
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  
-  app.use(routes);
-  
-  sequelize.sync({ force: false }).then(() => {
+    cookie: { 
+        maxAge: 1000 * 60 * 60 * 2
+     },
+     store: new SequelizeStore({
+        db:sequelize
+     })
+}))
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(routes)
+
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
   });
